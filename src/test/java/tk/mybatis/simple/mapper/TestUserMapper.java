97be6c73498f30a632440f50c0adcf2b857366ce
Little-Captain <coder.littlecaptain@gmail.com>
2019-07-25 00:23:25 +0800

    select 方法告一段落

--- src/test/java/tk/mybatis/simple/mapper/TestUserMapper.java
+++ src/test/java/tk/mybatis/simple/mapper/TestUserMapper.java
@@ -3,6 +3,7 @@ package tk.mybatis.simple.mapper;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Assert;
 import org.junit.Test;
+import tk.mybatis.simple.model.SysRole;
 import tk.mybatis.simple.model.SysUser;
 
 import java.util.List;
@@ -30,6 +31,18 @@ public class TestUserMapper extends TestBaseMapper {
             List<SysUser> users = userMapper.selectAll();
             Assert.assertNotNull(users);
             Assert.assertTrue(users.size() > 0);
+        }
+    }
+
+    @Test
+    public void testSelectRolesByUserId() {
+        try (SqlSession sqlSession = getSqlSession()) {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            List<SysRole> roles = userMapper.selectRolesByUserId(1L);
+            Assert.assertNotNull(roles);
+            Assert.assertEquals(2, roles.size());
+            Assert.assertEquals("admin", roles.get(0).getUser().getUsername());
+            Assert.assertEquals("admin", roles.get(1).getUser().getUsername());
         }
     }
 }
