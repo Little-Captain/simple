be330fa7bc0aa5eb864b958472700c82ee0d33a9
Little-Captain <coder.littlecaptain@gmail.com>
2019-07-25 00:01:00 +0800

    User 表查询

--- src/test/java/tk/mybatis/simple/mapper/TestCountryMapper.java
+++ src/test/java/tk/mybatis/simple/mapper/TestCountryMapper.java
@@ -1,37 +1,18 @@
 package tk.mybatis.simple.mapper;
 
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import tk.mybatis.simple.model.Country;
 
-import java.io.IOException;
-import java.io.Reader;
 import java.util.List;
 
-public class TestCountryMapper {
-
-    private static SqlSessionFactory sqlSessionFactory;
-
-    @BeforeClass
-    public static void init() {
-        try {
-            Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
-            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+public class TestCountryMapper extends TestBaseMapper {
 
     @Test
     public void testSelectAll() {
-        SqlSession sqlSession = sqlSessionFactory.openSession();
+        SqlSession sqlSession = getSqlSession();
         try {
-            List<Country> countryList = sqlSession.selectList("selectAll");
+            List<Country> countryList = sqlSession.selectList("tk.mybatis.simple.mapper.CountryMapper.selectAll");
             countryList
                     .stream()
                     .map(c -> String.format("%-4d%4s%4s", c.getId(), c.getName(), c.getCode()))
---
+++ src/test/java/tk/mybatis/simple/mapper/TestUserMapper.java
@@ -0,0 +1,35 @@
+package tk.mybatis.simple.mapper;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Assert;
+import org.junit.Test;
+import tk.mybatis.simple.model.SysUser;
+
+import java.util.List;
+
+public class TestUserMapper extends TestBaseMapper {
+
+    @Test
+    public void testSelectById() {
+        try (SqlSession sqlSession = getSqlSession()) {
+            // 获取 UserMapper 接口
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            // 调用 selectById 方法，查询 id = 1 的用户
+            SysUser user = userMapper.selectById(1L);
+            // user 不为空
+            Assert.assertNotNull(user);
+            // username = admin
+            Assert.assertEquals("admin", user.getUsername());
+        }
+    }
+
+    @Test
+    public void testSelectAll() {
+        try (SqlSession sqlSession = getSqlSession()) {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            List<SysUser> users = userMapper.selectAll();
+            Assert.assertNotNull(users);
+            Assert.assertTrue(users.size() > 0);
+        }
+    }
+}
