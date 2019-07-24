e40f812e0718301243420a66cf508d99f1740006
Little-Captain <coder.littlecaptain@gmail.com>
2019-07-24 15:16:59 +0800

    第1章 完成

---
+++ src/main/java/tk/mybatis/simple/model/Country.java
@@ -0,0 +1,32 @@
+package tk.mybatis.simple.model;
+
+public class Country {
+
+    private Long country_id;
+    private String country_name;
+    private String country_code;
+
+    public Long getCountry_id() {
+        return country_id;
+    }
+
+    public void setCountry_id(Long country_id) {
+        this.country_id = country_id;
+    }
+
+    public String getCountry_name() {
+        return country_name;
+    }
+
+    public void setCountry_name(String country_name) {
+        this.country_name = country_name;
+    }
+
+    public String getCountry_code() {
+        return country_code;
+    }
+
+    public void setCountry_code(String country_code) {
+        this.country_code = country_code;
+    }
+}
---
+++ src/test/java/tk/mybatis/simple/mapper/TestCountryMapper.java
@@ -0,0 +1,44 @@
+package tk.mybatis.simple.mapper;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import tk.mybatis.simple.model.Country;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+public class TestCountryMapper {
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+    @BeforeClass
+    public static void init() {
+        try {
+            Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testSelectAll() {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            List<Country> countryList = sqlSession.selectList("selectAll");
+            countryList
+                    .stream()
+                    .map(c -> String.format("%-4d%4s%4s", c.getCountry_id(), c.getCountry_name(), c.getCountry_code()))
+                    .forEach(System.out::println);
+        } finally {
+            // 不要忘记关闭 sqlSession
+            sqlSession.close();
+        }
+    }
+}
